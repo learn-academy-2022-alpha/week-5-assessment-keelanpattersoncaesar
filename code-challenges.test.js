b0e759bc325c6697d0c08d76d93c3b32b4a97149
keelanpattersoncaesar <keelanpattersoncaesar@gmail.com>
2022-03-22 08:38:11 -0700

    this is hard

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
 
+const { array } = require("yargs");
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -10,47 +12,125 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-
 // --------------------1) Create a function that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
+const secretCodeWord1 = "Lackadaisical";
 // Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
+const secretCodeWord2 = "Gobbledygook";
 // Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
+const secretCodeWord3 = "Eccentric";
 // Expected output: "3cc3ntr1c"
 
+describe("secretMessage", () => {
+  it("takes in a string and returns a coded message", () => {
+    const secretCodeWord1 = "Lackadaisical";
+    const secretCodeWord2 = "Gobbledygook";
+    const secretCodeWord3 = "Eccentric";
+
+    expect(secretMessage(secretCodeWord1)).toEqual("L4ck4d41s1c4l");
+    expect(secretMessage(secretCodeWord2)).toEqual("G0bbl3dyg00k");
+    expect(secretMessage(secretCodeWord3)).toEqual("3cc3ntr1c");
+  });
+});
+
+//   ReferenceError: secretMessage is not defined, buen fracaso
 
 // b) Create the function that makes the test pass.
-
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
-const letterA = "a"
+const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"];
+const letterA = "a";
 // Expected output: ["Apple", "Banana", "Orange"]
-const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
-const letterE = "e"
+const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"];
+const letterE = "e";
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
+describe("wordReturner", () => {
+  it("takes in an array of words and a single letter and returns all the words that contain that particular letter", () => {
+    const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"];
+    const letterA = "a";
+    const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"];
+    const letterE = "e";
+
+    expect(wordReturner(arrayOfWords1)).toEqual(["Apple", "Banana", "Orange"]);
+    expect(wordReturner(arrayOfWords2)).toEqual([
+      "Cherry",
+      "Blueberry",
+      "Peach",
+    ]);
+  });
+});
+
+// ReferenceError: wordReturner is not defined
 
 // b) Create the function that makes the test pass.
 
+// PSEUDOCODE:
+// create function called wordReturner
+// takes in array and single letter
+// create for loop to iterate
+// if statement
+// check each index for the letter provided
+// true, return the word
+// false, don't
+// return array
+
+// function wordReturner(array,value) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i].indexOf(value && value.toUpperCase())) {
+//             return array[i]
+//          }
+//     }
+// }
+
+// // Expected: ["Apple", "Banana", "Orange"]
+// // Received: "Apple"
+// // Not returning the rest of the words for some reason..
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
+const hand1 = [5, 5, 5, 3, 3];
 // Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
+const hand2 = [5, 5, 3, 3, 4];
 // Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
+const hand3 = [5, 5, 5, 5, 4];
 // Expected output: false
 
+describe("fullHouse", () => {
+  it("takes in an array of 5 numbers and determines whether or not the array is a 'full house'", () => {
+    const hand1 = [5, 5, 5, 3, 3];
+    const hand2 = [5, 5, 3, 3, 4];
+    const hand3 = [5, 5, 5, 5, 4];
+    expect(fullHouse(hand1)).toEqual([5, 5, 5, 3, 3]);
+    expect(fullHouse(hand2)).toEqual([5, 5, 3, 3, 4]);
+    expect(fullHouse(hand3)).toEqual([5, 5, 5, 5, 4]);
+  });
+});
+
+// ReferenceError: fullHouse is not defined, good fail
 
 // b) Create the function that makes the test pass.
+
+// PSEUDOCODE:
+// create fn named fullHouse
+    // takes in array
+// check array for 3 of same number && 2 of different number
+    // iterate using for loop
+    // slice and return true values 
+
+function fullHouse(array) {
+    let num1 = num
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == array.every(num1)) {
+            return array[i]
+        }
+        
+    }
+}
--- interview-questions.md
+++ interview-questions.md
@@ -7,53 +7,53 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a block in Ruby?
 
-  Your answer:
+  Your answer: A block is basically just Ruby's version of a function. 
 
-  Researched answer:
+  Researched answer: Blocks are just anonymous functions in Ruby. They have always have a 'do' AND an 'end' OR a set of { curly braces }. They can be passed into methods and can have multiple arguments. Parameters are passed through a set of | pipes | after the 'do'. You can also use built-in methods on blocks (for example, .each can be ran on an array of numbers).
 
 
 
 2. What is a gem?
 
-  Your answer:
+  Your answer: A gem is a collection of files that is used to install apps and dependencies that can be used and built upon with Ruby. Basically, it allows the developer to customize their Ruby experience
 
-  Researched answer:
+  Researched answer: A ruby gem is a collection of files that add a lot of functionality to Ruby. Technically, you can just write the code and custom methods into your program yourself, but gems are an easier way to implement them. For example, Rails is a ruby gem. Another popular gem is devise; it seamlessly implements an authentication solution (think sign-in, sign-out) to your program or website. 
 
 
 
 3. What is Ruby on Rails?
 
-  Your answer:
+  Your answer: Ruby on Rails is a Ruby-like language that allows developers to easily make queries and interact with relational databases. It is a very sensitive language that likes to be in control of all changes in the db. For example, something as simple as changing the name of a file needs to be done through the rails console; doing it another way will more than likely break your application and require you to start over.
 
-  Researched answer:
+  Researched answer: Ruby on Rails is a gem. Rails is a server-side fullstack web development tool that is used to facilitate queries and commands between Ruby and the backend database. Rails uses the MvC (model view controller) framework to interact with the user and update data on the fly. 
 
 
 
 4. What is a relational database? Are there other kinds of databases?
 
-  Your answer:
+  Your answer: I have never heard of a relational database. Time to do some research.
 
-  Researched answer:
+  Researched answer: A relational database is a collection of data items that have relationships which have been established in advance. In relational databases, data is usually stored in multiple tables and or columns and usually relies on SQL to perform queries on it. Other types of databases include NoSQL databases (doesn't use SQL toa access data), wide column databases, object oriented databases, key-value data bases, and more.
 
 
 
 5. What are primary keys? Why are they important?
 
-  Your answer:
+  Your answer: A primary key is the UNIQUE value that is assigned to each database entry. This allows a database to carry multiple entries that have the same value and, at the same time, allows each other those entries to be discovered individually by calling upon their primary key.
 
-  Researched answer:
+  Researched answer: Just as I explained before, a primary key is a unique value used to identify each entry in a database entry. Primary keys cannot be shared between different entries. They cannot contain null values and every entry MUST have a primary key.
 
 
 
 ## Looking Ahead: Terms for Next Week
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: A restful route provides mapping between HTTP verbs (GET, POST, PUT, etc), controllers, CRUD operations when dealing with database entries.
 
-2. JSON:
+2. JSON: Stands for JavaScript Object Notation; It is used for storing and transporting data when sending data from a server to a webpage
 
-3. ERB:
+3. ERB: Stands for eRuby; simply just a template that embeds Ruby into a text file
 
-4. Params:
+4. Params: An alias of the parameters method; 
 
-5. API:
+5. API: Stands for Application Programming Interface; Basically, the API is the connection between the server and the enduser's device. The API is what takes commands from the enduser, sends and gives direction to the server, then returns the result back to the enduser
